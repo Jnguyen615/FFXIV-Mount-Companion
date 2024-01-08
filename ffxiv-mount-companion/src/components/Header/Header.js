39e31666b3ecdc3a3d3ffb801717e81ab63d241b
Jen Nguyen <jenniferannnguyen@live.com>
2024-01-08 16:34:00 -0700

    Add component for header, logo and FFXIV logo

---
+++ ffxiv-mount-companion/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.scss';
+import './Header.scss';
+import Logo from '../Logo/Logo'
+
+const Header = () => {
+  return (
+    <header className='header'>
+    <h1 className='page-header'>FFXIV Mount Companion</h1>
+    <Logo /> 
+    </header>
+  )
+}
+
+export default Header
