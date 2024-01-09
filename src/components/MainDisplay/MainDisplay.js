b4d05b2619b17b67244d601d5a7ab400e3c183b9
Jen Nguyen <jenniferannnguyen@live.com>
2024-01-09 11:05:01 -0700

    Add stying to componenets are done

--- src/components/MainDisplay/MainDisplay.js
+++ src/components/MainDisplay/MainDisplay.js
@@ -1,10 +1,10 @@
+import './MainDisplay.scss'
 import NavBar from '../NavBar/NavBar';
-import MountCard from '../MountCard/MountCard'
+import MountCard from '../MountCard/MountCard';
 import Header from '../Header/Header';
-import FFXIVLogo from '../FFXIVLogo/FFXIVLogo'
+import FFXIVLogo from '../FFXIVLogo/FFXIVLogo';
 
 const MainDisplay = ({ mounts }) => {
-
   const mountCards = mounts.map(mount => (
     <div
       key={`${mount.id}-${mount.name}`}
@@ -21,12 +21,12 @@ const MainDisplay = ({ mounts }) => {
 
   return (
     <main className="main-display">
-      <Header /> 
-      <FFXIVLogo /> 
+      <Header />
+      <FFXIVLogo />
       <NavBar />
-      {mountCards}
+      <div className="mounts-container">{mountCards}</div>
     </main>
   );
 };
 
-export default MainDisplay
+export default MainDisplay;
