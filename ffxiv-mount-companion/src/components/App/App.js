3595805c7fffea59e342536f9f720a30a0863d14
Jen Nguyen <jenniferannnguyen@live.com>
2024-01-08 17:49:10 -0700

    Add api call

--- ffxiv-mount-companion/src/components/App/App.js
+++ ffxiv-mount-companion/src/components/App/App.js
@@ -1,8 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react'
 import Header from '../Header/Header';
 import FFXIVLogo from '../FFXIVLogo/FFXIVLogo';
+import { retrieveMounts } from '../../ApiCall'
 
 function App() {
+  const [mounts, setMounts] = useState([]);
+
+  useEffect(() => {
+    retrieveMounts()
+      .then(data => {
+        setMounts(data);
+        console.log("MOUNTS:", data);
+      })
+      .catch(error => {
+        console.error('Error fetching mounts data:', error);
+      });
+  }, []);
+
   return (
     <main className="app">
       <Header />
