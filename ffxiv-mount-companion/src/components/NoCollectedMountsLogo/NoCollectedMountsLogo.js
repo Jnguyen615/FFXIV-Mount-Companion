c75221d686b04df8c04054ec07f5170eac58b38d
Jen Nguyen <jenniferannnguyen@live.com>
2024-01-08 17:27:24 -0700

    Add components for the rest of the logos

---
+++ ffxiv-mount-companion/src/components/NoCollectedMountsLogo/NoCollectedMountsLogo.js
@@ -0,0 +1,12 @@
+import '.NoCollectedMountsLogo.scss'
+import FireBird from '../../Images/firebird-mount.webp'
+
+const NoCollectedMountsLogo = () => {
+  return (
+    <div className="firebird-container">
+      <img className="firebird" src={FireBird} alt="firebird" />
+    </div>
+  )
+}
+
+export default NoCollectedMountsLogo
